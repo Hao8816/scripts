7977fa6c2ec0462b07a87535ccb6a066dc36a16d
Vaster Chen <chenhao8816+8816@gmail.com>
2018-07-09 11:31:01 +0800

    Update tyc.user.js

--- tyc.user.js
+++ tyc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [外网版]［天眼查］ 获取公司详情
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.tianyancha.com/company/*
